217558189ccf1ef25c53e5e407e0eabedebc2015
Администратор <@ADMIN>
2014-08-26 20:25:41 +0300

    First 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AngularJS - Test</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        
+        <div class="row">
+            <h1 class="text-center">AngularJS - start</h1>
+        </div>
+        
+        <script type="text/javascript" src="js/angular.min.js"></script>
+    </body>
+</html>
